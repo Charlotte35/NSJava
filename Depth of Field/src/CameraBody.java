a7717794154f83499f66af2afb2f53353c8b95ee
Damien Pollet <damien.pollet@gmail.com>
2013-08-19 20:43:28 +0200

    Fix english spelling, reformat code
    Please do a Source>Format regularly, and at least before each commit :)

--- Depth of Field/src/CameraBody.java
+++ Depth of Field/src/CameraBody.java
@@ -1,36 +1,33 @@
-
 public class CameraBody {
-	
-	/*Connait le cercle de confusion acceptable pour le boitier*/
-	/*Choose the camera, film format or circle of confusion*/
+
+	/* Connait le cercle de confusion acceptable pour le boitier */
+	/* Choose the camera, film format or circle of confusion */
 	protected float circleOfConfusion;
-	
-	//Constructeur par défaut
-	public CameraBody(){
+
+	// Constructeur par défaut
+	public CameraBody() {
 		System.out.println("Création d'un boîtier");
-		circleOfConfusion=0f;
+		circleOfConfusion = 0f;
 	}
-	
-	//Constructeur avec des paramètres
-	public CameraBody(float circleOfConfusion){
+
+	// Constructeur avec des paramètres
+	public CameraBody(float circleOfConfusion) {
 		System.out.println("Création d'un boîtier avec des paramètres");
-		this.circleOfConfusion=circleOfConfusion;
+		this.circleOfConfusion = circleOfConfusion;
 	}
-	
-	//*************************ACCESSEURS************
-	
-	//Retourne le cercle de confusion
-	public float getCircleOfConfusion(){
+
+	// *************************ACCESSEURS************
+
+	// Retourne le cercle de confusion
+	public float getCircleOfConfusion() {
 		return circleOfConfusion;
 	}
-	
-	//****************MUTATEURS*****************
-	
-	//Définit le cercle de confusion
-	public void setCircleOfCondusion (float pCircleOfConfusion){
-		circleOfConfusion=pCircleOfConfusion;
+
+	// ****************MUTATEURS*****************
+
+	// Définit le cercle de confusion
+	public void setCircleOfCondusion(float pCircleOfConfusion) {
+		circleOfConfusion = pCircleOfConfusion;
 	}
-	
-		
 
 }
--- Depth of Field/src/Lens.java
+++ Depth of Field/src/Lens.java
@@ -1,62 +1,61 @@
-
 public class Lens extends CameraBody {
 
-	/*Connait les intervalles de valeur focale, f-number, focus distance*/
-	
-	/* Taille focale de l'objectif*/
-	protected int focaleLenght;
-	/* Ouverture de l'objectif*/
-	protected int aperture;
-	/* Distance du sujet*/
+	/* Connait les intervalles de valeur focale, f-number, focus distance */
+
+	/* Taille focale de l'objectif */
+	protected int focalLength;
+	/* Ouverture de l'objectif */
+	protected int maxAperture;
+	/* Distance du sujet */
 	protected int focusDistance;
-	
-	//Constructeur par défaut
-	public Lens(){
+
+	// Constructeur par défaut
+	public Lens() {
 		System.out.println("Création d'un objectif");
-		focaleLenght=0;
-		aperture=0;
-		focusDistance=0;
+		focalLength = 0;
+		maxAperture = 0;
+		focusDistance = 0;
 	}
-	
-	//Constructeur avec paramètres
-	public Lens(int focaleLenght, int aperture, int focusDistance){
+
+	// Constructeur avec paramètres
+	public Lens(int focaleLenght, int aperture, int focusDistance) {
 		System.out.println("Création d'un objectif avec des paramètres");
-		this.focaleLenght=focaleLenght;
-		this.aperture=aperture;
-		this.focusDistance=focusDistance;
+		this.focalLength = focaleLenght;
+		this.maxAperture = aperture;
+		this.focusDistance = focusDistance;
 	}
-	
-	//************************ACCESSEURS****************
-	
-	//Retourne la taille focale de l'objectif
-	public int getFocaleLenght(){
-		return focaleLenght;
+
+	// ************************ACCESSEURS****************
+
+	// Retourne la taille focale de l'objectif
+	public int getFocaleLenght() {
+		return focalLength;
 	}
-	
-	//Retourne l'ouverture de l'objectif
-	public int getAperture(){
-		return aperture;
+
+	// Retourne l'ouverture de l'objectif
+	public int getAperture() {
+		return maxAperture;
 	}
-	
-	//Retourne la distance du sujet
-	public int getFocusDistance(){
-		return focusDistance*1000;
+
+	// Retourne la distance du sujet
+	public int getFocusDistance() {
+		return focusDistance * 1000;
 	}
-	
-	//*********************MUTATEURS**********************
-	//Définit la taille de l'objectif
-	public void setFocaleLenght(int pFocaleLenght){
-		focaleLenght=pFocaleLenght;
+
+	// *********************MUTATEURS**********************
+	// Définit la taille de l'objectif
+	public void setFocaleLenght(int pFocaleLenght) {
+		focalLength = pFocaleLenght;
 	}
-	
-	//Définit l'ouverture de l'objectif
-	public void setAperture(int pAperture){
-		aperture=pAperture;
+
+	// Définit l'ouverture de l'objectif
+	public void setAperture(int pAperture) {
+		maxAperture = pAperture;
 	}
-	
-	//Définit la distance du sujet
-	public void setFocusDistance(int pFocusDistance){
-		focusDistance=pFocusDistance;
+
+	// Définit la distance du sujet
+	public void setFocusDistance(int pFocusDistance) {
+		focusDistance = pFocusDistance;
 	}
-	
+
 }
